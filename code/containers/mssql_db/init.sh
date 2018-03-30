6e1a6410b4ce76871a84e84bda346d0778154c6b
David T. Pheasant <david.pheasant@bluemetal.com>
2018-03-30 14:34:28 -0400

    Added -e option to sqlcmd to echo init script command

--- code/containers/mssql_db/init.sh
+++ code/containers/mssql_db/init.sh
@@ -21,7 +21,7 @@ echo $SQL_OUTPUT
 
 ## import the initial schema
 echo "[INFO] init.sh: Initalizing database..."
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -i $DB_INIT_SQL_FILE
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -e -i $DB_INIT_SQL_FILE
 
 ## if initial schema load was successful, wait on MSSQL server process (forever) if not, die.
 if [ $? -eq 0 ]; then
